4c583f989f68307884bbd060b010296a7713c575
Hadi Husain <34629636+hadi112@users.noreply.github.com>
2020-08-03 13:36:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port =process.env.port || 3000;
+const port =process.env.port;
 const users = require('./routes/api/users');
 const bodyparser = require('body-parser');
 const passport = require('passport');
